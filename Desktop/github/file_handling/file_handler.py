1e99b8cc4d02cdac69af65c902b3c94bc727b7f4
nicsalac <nicoletteirishsalac127@gmail.com>
2023-04-29 04:43:41 +0800

    even_integers.txt

--- Desktop/github/file_handling/file_handler.py
+++ Desktop/github/file_handling/file_handler.py
@@ -4,3 +4,12 @@
 with open("numbers.txt", "r") as input_file:
 # Read the contents of the file into a list of integers
     numbers = [int(line.strip()) for line in input_file]
+# Open the output files for writing
+with open("even.txt", "w") as even_file, open("odd.txt", "w") as odd_file:
+    # Loop over the numbers
+    for number in numbers:
+        # Check if the number is even
+        if number % 2 == 0:
+            even_file.write(str(number) + "\n")
+        else:
+            
