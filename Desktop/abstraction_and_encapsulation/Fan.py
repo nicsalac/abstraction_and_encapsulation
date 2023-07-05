67a35aa16cbb45bc6d9d2a5e321161ea015ec6dd
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 15:26:19 +0800

    get and set method for speed of fan

--- Desktop/abstraction_and_encapsulation/Fan.py
+++ Desktop/abstraction_and_encapsulation/Fan.py
@@ -3,3 +3,14 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    def init(self, speed=SLOW, radius=5, color='blue', on=False):
+        self.speed = speed
+        self.radius = radius
+        self.color = color
+        self.on = on
+    # get and set methods for class Fan
+    def get_speed(self):
+        return self.speed
+    def set_speed(self)
+        return self.speed 
