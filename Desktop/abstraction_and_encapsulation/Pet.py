522a009bc7da495a18f978a9a237193fba0f96f9
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-06 02:35:46 +0800

    methods that create those class Pet attributes

---
+++ Desktop/abstraction_and_encapsulation/Pet.py
@@ -0,0 +1,8 @@
+#create a class name Pet
+class Pet:
+    def __init__(self):
+        # methods that create the name, the animal type and age attributes
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
+        
