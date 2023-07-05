3020bb1d853e8e945fb1b55b75b8a33e5650bff5
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 21:57:56 +0800

    for loops to make the car accelerates 5 times

--- Desktop/abstraction_and_encapsulation/TestCar.py
+++ Desktop/abstraction_and_encapsulation/TestCar.py
@@ -2,3 +2,9 @@
 from Car import Car
 # create object for a class Car
 my_car = (2022, 'BMW')
+
+#accelerate the car five times
+for i in range(5):
+    my_car.accelerate()
+    print("car accelerating current speed:" , my_car.get_speed())
+    
