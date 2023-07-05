660376ee6dc7d968c44132e15552947512f5386a
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 16:55:51 +0800

    printing the output

--- Desktop/abstraction_and_encapsulation/TestFan.py
+++ Desktop/abstraction_and_encapsulation/TestFan.py
@@ -2,7 +2,10 @@
 from Fan import Fan
 
 # create two instances of the class Fan with diff parameters
-fan_no_1 = Fan(Fan.FAST, 10, 'yellow', True)
-fan_no_2 = Fan(Fan.MEDIUM,5,'blue',False)
+my_fan1 = Fan(Fan.FAST, 10, 'yellow', True)
+my_fan2 = Fan(Fan.MEDIUM,5,'blue',False)
 
+# print the attributes of 2 fans
+print("Fan 1 - Speed: {}, Radius: {}, Color: {}, Status: {}".format(my_fan1.get_speed(),my_fan1.get_radius(),my_fan1.get_color(),my_fan1.get_on()))
 
+print("Fan 2 - Speed: {}, Radius: {}, Color: {}, Status: {}".format(my_fan2.get_speed(),my_fan2.get_radius(),my_fan2.get_color(),my_fan2.get_on()))
