b898133e5ea1db4a838429505ae2c69e383eee98
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-06 02:48:53 +0800

    set the attributes of class Pet

--- Desktop/abstraction_and_encapsulation/Pet.py
+++ Desktop/abstraction_and_encapsulation/Pet.py
@@ -5,4 +5,10 @@ class Pet:
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
-        
+    #set the class Pet attributes
+    def set_name(self, name):
+        self.__name = name
+    def set_animal_type(self, animal_type)
+        self.__animal_type = animal_type
+    def set_age(self, age):
+        self.__age = age
