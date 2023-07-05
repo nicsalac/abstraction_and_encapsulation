87c8b01adb3e1b59ad13dd1c4dc8d72420069778
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 22:25:35 +0800

    braking the car five times

--- Desktop/abstraction_and_encapsulation/TestCar.py
+++ Desktop/abstraction_and_encapsulation/TestCar.py
@@ -1,10 +1,13 @@
 # importing class and modules
 from Car import Car
 # create object for a class Car
-my_car = (2022, 'BMW')
+my_car = Car(2022, 'BMW')
 
 #accelerate the car five times
 for i in range(5):
     my_car.accelerate()
     print("car accelerating current speed:" , my_car.get_speed())
-    
+#brake the car five times
+for i in range(5):
+    my_car.brake()
+    print("current speed when the car is braking:" , my_car.get_speed())
