42411236bb606d6050f5aee2850841ce69aa7514
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 21:09:49 +0800

    create methods for class and get the speed of the class Car

--- Desktop/abstraction_and_encapsulation/Car.py
+++ Desktop/abstraction_and_encapsulation/Car.py
@@ -4,4 +4,13 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+    # class Car methods
+    def accelerate(self)
+         self.__speed += 5
+    def brake(self):
+        self.__speed -= 5
+    def get_speed(self):
+        return self.__speed
         
+
+
