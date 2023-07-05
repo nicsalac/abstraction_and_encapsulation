fc75b2d3560696816f906136fc99724a60f25676
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 21:15:53 +0800

    final commit

--- Desktop/abstraction_and_encapsulation/TestFan.py
+++ Desktop/abstraction_and_encapsulation/TestFan.py
@@ -1,11 +1,11 @@
-#Importing class and modules
+# Importing class and modules
 from Fan import Fan
 
-# create two instances of the class Fan with diff parameters
 my_fan1 = Fan(Fan.FAST, 10, 'yellow', True)
-my_fan2 = Fan(Fan.MEDIUM,5,'blue',False)
+# my_fan2 = Fan(Fan.MEDIUM, 5, 'blue', False)
 
 # print the attributes of 2 fans
-print("Fan 1 - Speed: {}, Radius: {}, Color: {}, Status: {}".format(my_fan1.get_speed(),my_fan1.get_radius(),my_fan1.get_color(),my_fan1.get_on()))
+print("Fan 1 - Speed:", my_fan1.get_speed(), "Radius:", my_fan1.get_radius(), "Color:", my_fan1.get_color(), "Status:", my_fan1.get_on())
 
-print("Fan 2 - Speed: {}, Radius: {}, Color: {}, Status: {}".format(my_fan2.get_speed(),my_fan2.get_radius(),my_fan2.get_color(),my_fan2.get_on()))
+my_fan2 = Fan(Fan.MEDIUM, 5, 'blue', False)
+print("Fan 2 - Speed:", my_fan2.get_speed(), "Radius:", my_fan2.get_radius(), "Color:", my_fan2.get_color(), "Status:", my_fan2.get_on())  # corrected the print statements and added ',' between attributes
