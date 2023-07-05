79a64510ef31011f617461f16f61a1ee61e74d64
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-05 16:28:07 +0800

    labeling parameters for 2 Fan

--- Desktop/abstraction_and_encapsulation/TestFan.py
+++ Desktop/abstraction_and_encapsulation/TestFan.py
@@ -1,3 +1,8 @@
 #Importing class and modules
 from Fan import Fan
 
+# create two instances of the class Fan with diff parameters
+fan_no_1 = Fan(Fan.FAST, 10, 'yellow', True)
+fan_no_2 = Fan(Fan.MEDIUM,5,'blue',False)
+
+
