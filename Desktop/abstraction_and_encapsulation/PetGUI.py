46de350fa8821423e3c9cf483d50ed698ced7144
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-06 22:16:36 +0800

    applying GUI Components to class Pet

--- Desktop/abstraction_and_encapsulation/PetGUI.py
+++ Desktop/abstraction_and_encapsulation/PetGUI.py
@@ -1,3 +1,16 @@
 import tkinter as tk
+from tkinter import font
 #importing class and modules
 from Pet import Pet
+
+class PetGUIComponents:
+    #methods that create GUI components
+    def  __init__(self, window):
+        self.window = window
+        window.title("Pet Society")
+        window.geometry("500x300")
+        window.configure(bg= "#F3EFEF")
+
+
+
+
